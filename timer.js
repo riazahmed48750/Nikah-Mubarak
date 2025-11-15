180d88099ef2eae13c278a8ab6a716add6ad9735
riazahmed48750 <riazahmed48750@gmail.com>
2025-11-16 01:21:55 +0500

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,7 +1,4 @@
-
-// Set your Nikkah date here
 const targetDate = new Date("November 30, 2025 17:00:00").getTime();
-
 const countdown = setInterval(function() {
   const now = new Date().getTime();
   const distance = targetDate - now;
@@ -12,10 +9,10 @@ const countdown = setInterval(function() {
     return;
   }
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000*60*60*24));
+  const hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+  const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+  const seconds = Math.floor((distance % (1000*60)) / 1000);
 
   document.getElementById("days").innerText = days;
   document.getElementById("hours").innerText = hours;
